43e66f17fc2b3a07cfd11730d3b30d3307880312
m4tsu <matsurec0147@gmail.com>
2021-01-04 01:01:36 +0900

    レスポンシブ対応

--- src/components/layouts/Main.tsx
+++ src/components/layouts/Main.tsx
@@ -6,8 +6,8 @@ export const Main: FC = ({ children }) => {
   return (
     <main className="bg-primary py-10">
       <Container className="h-full">
-        <div className="flex w-full h-full rounded mx-auto justify-between">
-          <div className="rounded p-5 w-full mr-2 bg-primary text-white border-white border-4">
+        <div className="flex flex-col md:flex-row w-full h-full rounded mx-auto items-center md:items-start">
+          <div className="rounded p-5 w-full md:mr-2 bg-primary text-white border-white border-4 mb-5">
             {children}
           </div>
           <Aside />
